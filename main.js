32736ca402e51a78b559fa7ccdd470042bb3d106
Simone Cesano <simonecesano@libero.it>
2019-11-11 09:39:25 +0100

    Moved brush setup to app.js

--- main.js
+++ main.js
@@ -2,13 +2,5 @@
 var tool;
 window.onload = function() {
     paper.setup('canvas');
-    // Create a simple drawing tool:
-
-    tool = new Tool();
-
-    var brush = new Sharpie(paper.project)
-    tool.onMouseDown = brush.onMouseDown;
-    tool.onMouseUp = brush.onMouseUp;
-    tool.onMouseDrag = brush.onMouseDrag;
 }
 
